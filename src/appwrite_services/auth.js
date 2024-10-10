6b07e4c67ea7f04af40eb2441a59d98049e3c180
c-rew1nd <dreektseayger@gmail.com>
2024-10-10 15:53:00 +0530

    created authentication service with appwrite

---
+++ src/appwrite_services/auth.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-useless-catch */
+import conf from '../conf.js'
+import {Client, Account, ID} from "appwrite"
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another function
+                this.login({email, password});
+            } else{
+                return userAccount;
+            }
+        } catch (error) {
+            console.log("Appwrite service :: create account :: error", error);;
+        }
+    }
+
+    async login({email, password}){
+        try{
+            await this.account.createEmailPasswordSession(email, password);
+        } catch(error){
+            console.log("Appwrite service :: login :: error", error);
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        } catch (error){
+            throw error;
+        }
+        return null; // if there is no account
+    }
+
+    async logout(){
+        try{
+            await this.account.deleteSessions()
+        } catch (error){
+            console.log("Appwrite service :: login :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
