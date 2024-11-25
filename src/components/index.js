b60e5e67ae3444a2c443420651c8ab450fbbc9ee
c-rew1nd <dreektseayger@gmail.com>
2024-11-25 13:07:51 +0530

    created PostForm

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,8 @@ import Footer from "./Footer/Footer";
 import Container from "postcss/lib/container";
 import logo from './Logo'
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 
 export {Header, Footer
-    ,Container, logo, LogoutBtn
+    ,Container, logo, LogoutBtn, RTE
 }
