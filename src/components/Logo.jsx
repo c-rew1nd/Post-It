0627ec4d0e5cf9d13677d3a5b1d2faae5756538e
c-rew1nd <dreektseayger@gmail.com>
2025-01-22 14:26:58 +0530

    commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Logo({width = '100px'}) {
-  return (
-    <div>Logo</div>
+function Logo({width = '70px'}) {
+  return (   
+    <img src="https://img.freepik.com/free-vector/flat-design-atheism-logo-template_23-2149246666.jpg?t=st=1737534732~exp=1737538332~hmac=a8d72386bc4180ebfede808ad65265d946eaf5b3340739cb4e34188f67833c4d&w=740" alt="Logo" width={width}/>
   )
 }
 
